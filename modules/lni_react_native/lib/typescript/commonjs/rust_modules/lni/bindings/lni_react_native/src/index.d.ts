09d039b721b46e1062fddbfced542b15c0d6c4ef
nick <ntheile@gmail.com>
2025-09-30 19:17:42 -0500

    fix android package

--- modules/lni_react_native/lib/typescript/commonjs/rust_modules/lni/bindings/lni_react_native/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './generated/lni';
-declare const _default: {
-    lni: any;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
--- modules/lni_react_native/lib/typescript/commonjs/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './generated/lni';
-import * as lni from './generated/lni';
-declare const _default: {
-    lni: typeof lni;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
